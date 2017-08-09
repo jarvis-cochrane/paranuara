8ca1a1c34c28988e56d62322cdb2c60d6665d4b5
Jarvis Cochrane <jarvis@cochrane.com.au>
2017-08-09 21:02:41 +1000

    Add additional import methods and test cases. Refactor exception handling in imports.

--- paranuara_api/importers.py
+++ paranuara_api/importers.py
@@ -2,9 +2,21 @@ import json
 import logging
 import sys
 
-from paranuara_api.models import Company
+from paranuara_api.models import Company, Tag, Foodstuff
 
 logger = logging.getLogger(__name__)
+
+# TODO: Refactor the exception handling and logging as a decorator
+#       which could be used like this:
+#
+# @log_exception('Error creating company')
+# def _import_company(data):
+#     return Company.objects.create(...)
+#
+def _log_exception(msg, e):
+        e_type, e_value, _ = sys.exc_info()
+        logger.error('%s (%s: %s)', msg, e_type, e_value)
+
 
 def _import_company(data):
     """
@@ -12,18 +24,15 @@ def _import_company(data):
     in the supplied dictionary `data`.
     `data` must contain the keys `index` and `company`
     """
-    logger.debug('_make_company(): %s', data)
+    logger.debug('_import_company(): %s', data)
 
     # TODO: More detailed validation and error reporting
     try:
-        company = Company()
-        company.index = data['index']
-        company.company_name = data['company']
-        company.save()
-        return company
+        return Company.objects.create(
+                index=data['index'], 
+                company_name=data['company'])
     except Exception as e:
-        e_type, e_value, _ = sys.exc_info()
-        logger.error('Error creating company (%s: %s)', e_type, e_value)
+        _log_exception('Error creating company', e)
     return None
 
 
@@ -31,3 +40,37 @@ def import_companies(fp):
     logger.info('Importing companies')
 
     return json.load(fp, object_hook=_import_company)
+
+
+def _import_tag(label):
+    """
+    Return a Tag instance for the supplied `label` value, creating it
+    if it doesn't exist.
+    """
+    logger.debug('_import_tag(): %s', label)
+    try:
+        (tag, _) =  Tag.objects.get_or_create(
+                        label=label, 
+                        defaults={'label': label})
+        return tag
+    except Exception as e:
+        _log_exception('Error importing tag', e)
+    return None
+
+
+def _import_foodstuff(name):
+    """
+    Return a Foodstuff instance for the supplied `name` value, creating it
+    if it doesn't exist. Newly created Foodstuff instances will be assigned
+    type='Vegetable'.
+    """
+    logger.debug('_import_foodstuff(): %s', name)
+    try:
+        (foodstuff, _) = Foodstuff.objects.get_or_create(
+                            name=name, 
+                            defaults={'name': name, 
+                                      'type': Foodstuff.VEGETABLE})
+        return foodstuff
+    except Exception as e:
+        _log_exception('Error importing foodstuff', e)
+    return None
--- paranuara_api/tests/test_importers.py
+++ paranuara_api/tests/test_importers.py
@@ -1,7 +1,12 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from paranuara_api.importers import _import_company, import_companies
+from paranuara_api.importers import (
+        _import_company, import_companies, _import_tag,
+        _import_foodstuff
+)
+from paranuara_api.models import Foodstuff, Tag
+
 
 # TODO: More extensive test cases
 
@@ -33,3 +38,49 @@ class ImportCompaniesTestCase(TestCase):
             companies = import_companies(fp)
 
         self.assertEquals(len(companies), 100)
+
+
+class ImportTagTestCase(TestCase):
+
+    def test_import_tag_new(self):
+        self.assertFalse(Tag.objects.exists())
+
+        tag = _import_tag('lame')
+
+        self.assertEqual(len(Tag.objects.all()), 1)
+        self.assertIsNotNone(tag.id)
+        self.assertEquals(tag.label, 'lame')
+
+    def test_import_tag_exists(self):
+        expected_tag = Tag.objects.create(label='tame')
+        self.assertEqual(len(Tag.objects.all()), 1)
+
+        tag = _import_tag('tame')
+
+        self.assertEqual(len(Tag.objects.all()), 1)
+        self.assertEquals(tag.label, 'tame')
+
+
+class ImportFoodstuffTestCase(TestCase):
+
+    def test_import_foodstuff_new(self):
+        self.assertFalse(Foodstuff.objects.exists())
+
+        foodstuff = _import_foodstuff('leek')
+
+        self.assertEqual(len(Foodstuff.objects.all()), 1)
+        self.assertIsNotNone(foodstuff.id)
+        self.assertEquals(foodstuff.name, 'leek')
+        self.assertEquals(foodstuff.type, Foodstuff.VEGETABLE)
+
+    def test_import_foodstuff_exists(self):
+        expected_foodstuff = Foodstuff.objects.create(
+                                name='apple', 
+                                type=Foodstuff.FRUIT)
+        self.assertEqual(len(Foodstuff.objects.all()), 1)
+
+        foodstuff = _import_foodstuff('apple')
+
+        self.assertEqual(len(Foodstuff.objects.all()), 1)
+        self.assertEquals(foodstuff.name, 'apple')
+        self.assertEquals(foodstuff.type, Foodstuff.FRUIT)
